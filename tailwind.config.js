846531c743d0bef311b139c6925b061a6ba408a3
yerin <dev.yerin@gmail.com>
2024-11-22 13:38:40 +0900

    fix: tailwindcss cli -> vite 설정으로 변경

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
-// import type { Config } from "tailwindcss";
-
+/** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {},
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
   plugins: [],
-}
+};
