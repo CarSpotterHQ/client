f25a05d24804db5332b8bd94d3f4dc45896bea5b
yerin <dev.yerin@gmail.com>
2024-12-23 17:53:00 +0900

    chore: svgr 설정 코드 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr({ include: '**/*.svg' })],
   resolve: {
     // eslint-disable-next-line no-undef
     alias: [{ find: '@/', replacement: path.resolve(__dirname, './src/*') }],
