b9943e60f1eb0327506bc1a26d0b0d6740395fd2
bohodays <bohodays0316@gmail.com>
2025-01-21 11:39:39 +0000

    Merge pull request #9 from CarSpotterHQ/feature/api
    [FEAT] api 폴더 구성 및 axios 기본 설정

---
+++ store/authStore.ts
@@ -0,0 +1,24 @@
+import { create } from "zustand";
+
+type User = {
+  username: string;
+  email: string;
+};
+
+type AuthState = {
+  isLoggedIn: boolean;
+  user: User | null;
+  login: (userData: User) => void;
+  logout: () => void;
+};
+
+const useAuthStore = create<AuthState>((set) => ({
+  isLoggedIn: false,
+  user: null,
+
+  login: (userData) => set({ isLoggedIn: true, user: userData }),
+
+  logout: () => set({ isLoggedIn: false, user: null }),
+}));
+
+export default useAuthStore;
