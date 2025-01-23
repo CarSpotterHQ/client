ca84a6b013ad982446e3991919f43475960b81b4
pjw2369 <pjw2369@lgcns.com>
2025-01-23 10:25:33 +0900

    feat: ssr router 설정

--- src/router.tsx
+++ src/router.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import ProtectedLayout from './../layout/ProtectedLayout';
+import { Route, Routes } from 'react-router';
 
 // 임시로 생성
 const HomePage = () => <h1>홈페이지</h1>;
 const LoginPage = () => <h1>로그인 페이지</h1>;
-const ProtectedPage = () => <h1>로그인 후 접근 가능한 페이지</h1>;
 
 const Router = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* public route */}
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-
-        {/* protected route */}
-        <Route element={<ProtectedLayout />}>
-          <Route path="/protected" element={<ProtectedPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
   );
 };
 
