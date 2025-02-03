cfd011dd006f7ec4d7bdafc51cc863e6deb6d883
bohodays <bohodays0316@gmail.com>
2025-02-03 23:17:35 +0900

    feat: 퍼블리싱 작업 세팅

---
+++ src/components/penalty/TimeSetting.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TimeSetting = ({ _onNext }: any) => {
+  return <div>TimeSetting</div>;
+};
+
+export default TimeSetting;
--- src/router.tsx
+++ src/router.tsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import { Route, Routes } from 'react-router';
+import PenaltyPage from '@//pages/PenaltyPage';
+import Layout from './layout/Layout';
 
 const Router = () => {
   return (
     <Routes>
-      {/* 영률님 파트 */}
-      <Route path="/" element={<div>영률님 파트</div>} />
+      <Route element={<Layout />}>
+        {/* 영률님 파트 */}
+        <Route path="/" element={<div>영률님 파트</div>} />
 
-      {/* 중원 파트 */}
-      <Route path="/jungwon" element={<div>중원 파트</div>} />
+        {/* 중원 파트 */}
+        <Route path="/penalty" element={<PenaltyPage />} />
 
-      {/* 예린님 파트 */}
-      <Route path="/yerin" element={<div>예린님 파트</div>} />
+        {/* 예린님 파트 */}
+        <Route path="/yerin" element={<div>예린님 파트</div>} />
 
-      <Route path="*" element={<div>Not Found</div>} />
+        <Route path="*" element={<div>Not Found</div>} />
+      </Route>
     </Routes>
   );
 };
